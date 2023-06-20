d8a42021338c9aef1963b79c7e9f1fd2abeea68c
eel <eeldigit@gmail.com>
2023-06-20 09:50:17 +0200

    add welcomScreen

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import {
+//   Platform,
+//   View,
+//   StatusBar,
+//   StyleSheet,
+//   TouchableWithoutFeedback,
+//   Image,
+//   TouchableOpacity,
+//   Button,
+//   TouchableHighlight,
+// } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App() {
+//   // console.log("app running");
+
+//   return (
+//     <View style={styles.containerView}>
+//       {/* <View
+//         style={{
+//           backgroundColor: "teal",
+//           width: 100,
+//           height: 100,
+//           alignSelf: "center",
+//         }}
+//       />
+//       <View
+//         style={{
+//           backgroundColor: "dodgerblue",
+//           width: 100,
+//           height: 200,
+//           alignSelf: "center",
+//         }}
+//       />
+//       <View
+//         style={{
+//           backgroundColor: "tomato",
+
+//           width: 100,
+//           height: 300,
+//           alignSelf: "center",
+//         }}
+//       /> */}
+//       {/* <Text  numberOfLines={1}>  Sale it  all ! voici un text très long et je le veux encore plus long </Text> */}
+
+//       <TouchableOpacity onPress={() => console.log("image tapped")}>
+//         <Image
+//           source={{
+//             width: 200,
+//             height: 300,
+//             uri: "https://picsum.photos/id/237/200/300",
+//           }}
+//         />
+//       </TouchableOpacity>
+
+//       <TouchableHighlight>
+//         <Button
+//           title="Click me !"
+//           color="orange"
+//           onPress={() => console.log("click")}
+//         />
+//       </TouchableHighlight>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   containerView: {
+//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+//     backgroundColor: "red",
+//     flex: 1,
+//     flexDirection: "row",
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+// });
+
+// // ======================================================
+
+import React from "react";
+import WelcomSreen from "./app/screens/WelcomSreen";
+
+function App(props) {
+  return <WelcomSreen />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ app/screens/WelcomSreen.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Text } from "react-native";
+import { ImageBackground, StyleSheet, View, Image } from "react-native";
+
+function WelcomSreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text> Sell what you don't need </Text>
+      </View>
+
+      <View style={styles.loginButton} />
+      <View style={styles.registerButton} />
+    </ImageBackground>
+  );
+}
+
+export default WelcomSreen;
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ECDC4",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    alignItems: "center",
+    top: 70,
+  },
+});
